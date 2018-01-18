2f6df6ddfac3b5b76303fde7ed6bf0e56952ac9b
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-01-18 19:55:01 +0100

    Add a function Column in js

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,37 @@ $(function() {
 	    return str;
 	};
 
+	function Column(name) {
+	    var self = this; // useful for nested functions
+
+	    this.id = randomString();
+	    this.name = name;
+	    this.$element = createColumn();
+
+	    function createColumn() {
+	    	var $column = $('<div>').addClass('column');
+			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+			var $columnCardList = $('<ul>').addClass('column-card-list');
+			var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+			var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+	    //
+	    $columnDelete.click(function() {
+        	self.removeColumn();
+		});
+	    //
+		$columnAddCard.click(function() {
+        	self.addCard(new Card(prompt("Enter the name of the card")));
+		});
+		//
+		$column
+			.append($columnTitle)
+       		.append($columnDelete)
+        	.append($columnAddCard)
+        	.append($columnCardList);
+        //	
+		return $column;
+		}
+	}
+	//
+
 });	
