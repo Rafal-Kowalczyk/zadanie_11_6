5cf2ab9d3e261732dbff1570230958c9f465d1e3
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-01-18 19:56:38 +0100

    Add a function Card in js

--- js/scripts.js
+++ js/scripts.js
@@ -50,5 +50,29 @@ $(function() {
 	    }
 	};
 	//
+	function Card(description) {
+		var self = this;
+
+	    this.id = randomString();
+	    this.description = description;
+	    this.$element = createCard();
+
+	    function createCard() {
+	    	var $card = $('<li>').addClass('card');
+		    var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+		    var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+	    //
+	    $cardDelete.click(function(){
+        	self.removeCard();
+		});
+
+		$card
+			.append($cardDelete)
+			.append($cardDescription);
+
+		return $card;
+		}
+	}
+	//
 
 });	
